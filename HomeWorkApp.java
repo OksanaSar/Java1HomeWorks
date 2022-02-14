a4bb38d300f36fdbb97d63b2c719a09a798a0fe4
OksanaSar <99191209+OksanaSar@users.noreply.github.com>
2022-02-14 16:05:22 +0400

    Add files via upload

---
+++ HomeWorkApp.java
@@ -0,0 +1,37 @@
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    static void checkSumSign() {
+        int a = 20,b = 30;
+        System.out.println(a + b >= 0? "Positive sum" : "Negative sum");
+    }
+
+    static void printColor(){
+        int value = -3;
+        if (value <= 0) {
+            System.out.println("Red");
+        }
+        else if (value <= 100) {
+            System.out.println("Yellow");
+        }
+        else {
+            System.out.println("Green");
+        }
+    }
+
+    static void compareNumbers() {
+        int a = 2, b = 2;
+        System.out.println (a >= b? "a >= b" : "a < b");
+    }
+}
